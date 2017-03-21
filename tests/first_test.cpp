05a10e57c942b0c92f3646daad8d2b810a042c17
Boris Popov <popov@whitekefir.ru>
2017-03-21 13:20:59 +0300

    fix tests CMakeLists.txt for rapidjson

--- tests/first_test.cpp
+++ tests/first_test.cpp
@@ -1,10 +1,19 @@
 //#include <file/NameGenerator.hpp>
+#include <rapidjson/reader.h>
 #include <gtest/gtest.h>
 
-TEST(first, test_001)
+TEST(first, test_01)
 {
+     ASSERT_TRUE(true);
      //using namespace file;
      //NameGenerator ng(2, 1);
      //ASSERT_EQ("download_0.xml", ng.name());
+}
 
+TEST(first, test_02)
+{
+     //ASSERT_TRUE(true);
+     //using namespace file;
+     //NameGenerator ng(2, 1);
+     //ASSERT_EQ("download_0.xml", ng.name());
 }
