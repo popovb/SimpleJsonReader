6f2856691a48ddc9b8381cff981e612103af1d3b
Boris Popov <popov@whitekefir.ru>
2017-03-21 12:58:05 +0300

    add first test

---
+++ tests/first_test.cpp
@@ -0,0 +1,10 @@
+//#include <file/NameGenerator.hpp>
+#include <gtest/gtest.h>
+
+TEST(first, test_001)
+{
+     //using namespace file;
+     //NameGenerator ng(2, 1);
+     //ASSERT_EQ("download_0.xml", ng.name());
+
+}
