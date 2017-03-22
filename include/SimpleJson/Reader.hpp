7fa94749382d5276a6a938e917132a35db3dbcdb
Boris Popov <popov@whitekefir.ru>
2017-03-22 16:59:14 +0300

    add dumb for Reader

---
+++ include/SimpleJson/Reader.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef __Reader_hpp__
+#define __Reader_hpp__
+
+namespace simple_json {
+//////////////////////////////////////////////////////////////////
+     class Reader {
+
+     public:
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __Reader_hpp__
