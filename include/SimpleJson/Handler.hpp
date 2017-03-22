5254f0db12b98b96a619334066d5e3f3882e2c4e
Boris Popov <popov@whitekefir.ru>
2017-03-22 13:51:17 +0300

    add dumb Handler

---
+++ include/SimpleJson/Handler.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef __Handler_hpp__
+#define __Handler_hpp__
+
+namespace simple_json {
+//////////////////////////////////////////////////////////////////
+     class Handler {
+	  //
+	  //TODO
+	  //
+     };
+//////////////////////////////////////////////////////////////////
+}
+#endif // __Handler_hpp__
