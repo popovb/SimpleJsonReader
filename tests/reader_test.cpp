85f88d867bcab929ca62a692c5ae14a70d9c4d75
Boris Popov <popov@whitekefir.ru>
2017-03-23 16:16:44 +0300

    add unit test for Reader

--- tests/reader_test.cpp
+++ tests/reader_test.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 TEST(handler, test_01)
 {
@@ -63,4 +65,90 @@ TEST(handler, test_04)
      Reader R;
      MH MHH;
      ASSERT_TRUE(R.Parse(in, MHH));
+}
+
+class MH2: public simple_json::Handler {
+
+public:
+     MH2(): counter(0) { };
+     virtual void ObjectStart() {
+	  counter++;
+	  std::cout << "object start\n";
+     }
+
+     virtual void ObjectEnd() {
+	  counter++;
+	  std::cout << "object end\n";
+     }
+
+     virtual void ArrayStart() {
+	  counter++;
+	  std::cout << "array start\n";
+     }
+
+     virtual void ArrayEnd() {
+	  counter++;
+	  std::cout << "array end\n";
+     };
+
+     virtual void Key(const simple_json::key_t) {
+	  counter++;
+	  switch (counter) {
+	  case 2:
+	  case 4:
+	  case 6:
+	  case 8:
+	  case 10:
+	  case 12:
+	  case 14:
+	  case 17:
+	       return;
+	  default:
+	       throw std::runtime_error("key");
+	  }
+     };
+
+     virtual void Value(const simple_json::value_t) {
+	  counter++;
+	  switch (counter) {
+	  case 3:
+	  case 5:
+	  case 7:
+	  case 9:
+	  case 11:
+	  case 15:
+	  case 19:
+	  case 20:
+	  case 21:
+	  case 22:
+	  case 23:
+	       return;
+	  default:
+	       throw std::runtime_error("value");
+	  }
+     }
+
+private:
+     int counter;
+};
+
+TEST(handler, test_05)
+{
+     using namespace simple_json;
+     std::string in = R"raw(
+{
+    "hello7" : "world7",
+    "t"      : true ,
+    "f"      : false,
+    "n"      : null,
+    "i"      : 123,
+    "pi"     : { "x" : 6 },
+    "pi2"    : [ "x", 6, 777.77, true, "true" ]
+}
+
+)raw";
+     std::stringstream in2(in);
+     Reader R;
+     MH2 MHH;
+     ASSERT_TRUE(R.Parse(in2, MHH));
 }
